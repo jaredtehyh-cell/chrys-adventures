79e27468e2acc1752655ce79c6008d86b3cca434
Jared <jaredtehyh@gmail.com>
2026-06-26 23:31:00 +0800

    Fix GitHub Pages routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 
 export default function App() {
+  const basename = import.meta.env.PROD ? '/chrys-adventures' : '/'
+
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<LandingPage />} />
       </Routes>
